b28309dfd536855e3094a28e8452e39867accf2a
Ethan <ethannorman@outlook.com>
2018-07-27 20:48:01 -0500

    Add project files.

---
+++ MvcMusicStore/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ MvcMusicStore/Models/Review.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMusicStore.Models
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+
+        public int AlbumID { get; set; }
+        public virtual Album Album { get; set; }
+
+        public string Contents { get; set; }
+
+
+        [DataType(DataType.EmailAddress)]
+        public string ReviewerEmail { get; set; }
+    }
+}
